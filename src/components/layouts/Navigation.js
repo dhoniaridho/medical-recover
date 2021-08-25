6a07858e64f5c497a124a789cf5407d5a5c716f1
dhoniaridho <dhoniaridho@gmail.com>
2021-08-25 12:18:20 +0700

    project init

---
+++ src/components/layouts/Navigation.js
@@ -0,0 +1,16 @@
+export default function Nevigation () {
+    return (
+        <header>
+            <nav>
+                <div className="flex justify-between">
+                    <div>
+                        
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
