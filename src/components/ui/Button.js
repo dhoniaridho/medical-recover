2c48dd2d0bf7bade5c30dda61a9df6f55a2adec4
dhoniaridho <dhoniaridho@gmail.com>
2021-08-25 16:59:15 +0700

    created base dept

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,7 +2,7 @@ import classNames from "classnames"
 
 export default function Button (props) {
     const {children, type, className} = props
-    const buttonClass = classNames('px-5 py-2 rounded-md font-semibold text-sm', className,{
+    const buttonClass = classNames('px-5 py-2 font-semibold text-sm', className,{
         'bg-red-500 text-white hover:bg-red-600 border border-red-500' : type === 'primary',
         'bg-white text-red-500 border-red-500 border hover:bg-red-500 hover:text-white' : type === 'secondary',
     })
--- src/views/index.js
+++ src/views/index.js
@@ -60,12 +60,44 @@ export default function Index() {
                         <p className="mt-5 w-3/4">Problems trying to resolve the conflict between
                             the two major realms of Classical physics:
                             Newtonian mechanics </p>
-                       <div className="mt-5">
-                       <a className="text-red-500 font-semibold ">Learn More ></a>
-                       </div>
+                        <div className="mt-5">
+                            <a href="/" className="text-red-500 font-semibold ">Learn More ></a>
+                        </div>
                     </div>
                     <div className="w-1/2">
-                        <img src="https://i.ibb.co/JtKx1TL/meet.png" />
+                        <img src="https://i.ibb.co/JtKx1TL/meet.png" alt="meet" />
+                    </div>
+                </div>
+            </section>
+            <section className="py-20 px-28 bg-green-50">
+                <div>
+                    <h4 className="text-red-500 font-bold">Pratice Advice</h4>
+                    <h1 className="text-4xl font-bold">Our Departement</h1>
+                    <p className="text-sm w-1/2 mt-6">Problems trying to resolve the conflict between
+                        the two major realms of Classical physics: Newtonian mechanics </p>
+                </div>
+                <div className="grid grid-cols-3 py-10">
+                    <div className="bg-white">
+                        <div className="">
+                            <img src="https://i.ibb.co/jbJ7mcz/dep-1.png" />
+                        </div>
+                        <div>
+                        <div className="p-4">
+                        <div className="flex justify-between mt-3">
+                            <h5 className="text-red-500 font-semibold">Painless procedures</h5>
+                            <div className="bg-gray-900 text-white rounded-full px-3 py-1 text-sm">4.9</div>
+                        </div>
+                            <h3 className="text-3xl font-bold">Quick examination</h3>
+                            <p className="text-sm my-3">We focus on ergonomics and meeting
+                                you where you work. It's only a
+                                keystroke away.</p>
+                            <div className="text-gray-600">15 Sales</div>
+                            <div className="text-gray-400 flex font-bold"><h5>$16.48</h5><h5 className="text-green-700">$6.48</h5></div>
+                        <Button className="rounded-full border-red-500 border mt-3">
+                            Learn More
+                        </Button>
+                        </div>
+                        </div>
                     </div>
                 </div>
             </section>
